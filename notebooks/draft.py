e1ac1ab6b00dbb643ff8d4c0a5b91dbc7daa503b
tnathu-ai <ngocanhthu20102002@gmail.com>
2022-08-22 14:09:56 +1000

    drop cols

---
+++ notebooks/draft.py
@@ -0,0 +1,11 @@
++ Import python libraries and dataset
+
++ Merge 2 dataframe (atheletes & leaderboard_15)
+
++ Check and drop duplicated rows
+
++ Add labels to data
+
++ Make athlete_id unique
+
++ Drop meaningless columns or unecessary columns: 'retrieved_datetime_x', 'retrieved_datetime_y', 'year', 'stage', 'scaled'
