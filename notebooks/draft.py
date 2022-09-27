e74b76940caf7d0d8f9a5bccdf6960a3b35349b7
tnathu-ai <ngocanhthu20102002@gmail.com>
2022-09-27 16:16:26 +1000

    cleaned files and folders

--- notebooks/draft.py
+++ notebooks/draft.py
@@ -1,11 +1,29 @@
-+ Import python libraries and dataset
+def coerce_df_columns_to_best_dtype(df, numeric_column_list, datetime_column_list):
+    # convert Id to integer and Salary to float datatype
+    df[numeric_column_list] = df[numeric_column_list].apply(pd.to_numeric, errors='coerce')
+    # convert to datetime format
+    df[datetime_column_list] = df[datetime_column_list].apply(pd.to_datetime, errors='coerce')
+    # convert object columns to string datatype
+    df = df.convert_dtypes()
+    # return datatype for each column after coercing
+    # select numeric columns
+    df_numeric = df.select_dtypes(include=[np.number]).columns.to_list()
 
-+ Merge 2 dataframe (atheletes & leaderboard_15)
+    # select non-numeric columns
+    df_string = df.select_dtypes(include='string').columns.tolist()
 
-+ Check and drop duplicated rows
+    # print number of numeric column
+    print("Number of numeric columns: ", len(df_numeric))
+    print("List of numeric columns: ", df_numeric, "\n")
 
-+ Add labels to data
+    print("Number of categorical columns: ", len(df_string))
+    print("List of string columns: ", df_string, "\n\n")
 
-+ Make athlete_id unique
+    return df.info()
 
-+ Drop meaningless columns or unecessary columns: 'retrieved_datetime_x', 'retrieved_datetime_y', 'year', 'stage', 'scaled'
+coerce_df_columns_to_best_dtype(df, ['Id','Salary'], ['OpenDate', 'CloseDate'])
+
+# female weight variance
+
+
+
